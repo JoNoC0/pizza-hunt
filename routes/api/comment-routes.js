b55f385d6e87f9e6fcbfb8a266436589bab360d7
JoNoC0 <jnguyeningco@gmail.com>
2022-01-19 14:11:03 -0600

    add-replies

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,27 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controllers');
+const { 
+    addComment, 
+    removeComment,
+    addReply,
+    removeReply
+    } = require('../../controllers/comment-controllers');
 
 // /api/comments/<pizzaId>
-router.route('/:pizzaId').post(addComment);
+//  post route to addComment
+router
+    .route('/:pizzaId')
+    .post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+// delete route to removeComment
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
 
+// delete route to removeReply
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
+    
 module.exports = router;
